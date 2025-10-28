1e0a2a897704f08b2e8c182b9e6dafce61de0177
FakeZero(Laptop) <sjlimdaa@naver.com>
2025-10-28 17:24:28 +0900

    grass 클래스가 이미지를 한 번만 로드하고 인스턴스 생성시 y값을 받을 수 있도록 수정

--- grass.py
+++ grass.py
@@ -2,11 +2,15 @@ from pico2d import load_image
 
 
 class Grass:
-    def __init__(self):
-        self.image = load_image('grass.png')
+    image = None
+
+    def __init__(self, y = 30):
+        if self.image is None:
+            self.image = load_image('grass.png')
+        self. y = y
 
     def draw(self):
-        self.image.draw(400, 30)
+        self.image.draw(400, self.y)
 
     def update(self):
         pass
